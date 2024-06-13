c327c46ff85a210029284d9dc60448d0b1f02fce
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-06-13 15:58:19 -0400

    uses production data endpoint

--- src/data-sources/ost.js
+++ src/data-sources/ost.js
@@ -3,7 +3,7 @@ export default {
   type: 'http-get',
   dependent: 'none',
   resettable: false,
-  url: 'https://stage.cityspan.com:8080/phillyost/service/getall',
+  url: 'https://api.cityspan.com/phillyost/service/getall',
   options: {
     success: function(data) {
       console.log('ost.js success is running, data:', data);
@@ -16,10 +16,5 @@ export default {
       }
       return newData;
     },
-    // params: {
-    //   where: '1=1',
-    //   outFields: '*',
-    //   f: 'pjson',
-    // },
   },
 };
