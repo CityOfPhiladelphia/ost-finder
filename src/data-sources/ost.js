8af33874c89cd4a8d44c0642bf6ae5acfd84c28f
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-11-26 10:11:32 -0500

    uses new staging endpoint

--- src/data-sources/ost.js
+++ src/data-sources/ost.js
@@ -3,16 +3,17 @@ export default {
   type: 'http-get',
   dependent: 'none',
   resettable: false,
-  url: 'https://api.cityspan.com/phillyost/service/getall',
+  url: 'https://stage.cityspan.com:8080/phillyost/service/getall',
+  // url: 'https://api.cityspan.com/phillyost/service/getall',
   options: {
     success: function(data) {
       console.log('ost.js success is running, data:', data);
-      let newData = { features: [] };
+      let newData = { features: []};
       for (let datum of data) {
         newData.features.push({
           attributes: datum,
           latlng: [ datum.latitude, datum.longitude ],
-        })
+        });
       }
       return newData;
     },
