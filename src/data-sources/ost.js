db65caaf1932d34dec81f23ab09ea7df4e8bc640
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-01-02 16:46:48 -0500

    uses production endpoint again

--- src/data-sources/ost.js
+++ src/data-sources/ost.js
@@ -3,8 +3,8 @@ export default {
   type: 'http-get',
   dependent: 'none',
   resettable: false,
-  url: 'https://stage.cityspan.com:8080/phillyost/service/getall',
-  // url: 'https://api.cityspan.com/phillyost/service/getall',
+  // url: 'https://stage.cityspan.com:8080/phillyost/service/getall',
+  url: 'https://api.cityspan.com/phillyost/service/getall',
   options: {
     success: function(data) {
       console.log('ost.js success is running, data:', data);
