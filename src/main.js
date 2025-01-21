179420ff3466e65f3dbf47f35e9227f18d02adb0
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-01-21 15:25:16 -0500

    uses pinboard 2.1.12, sets up dev and prod pushes to s3

--- src/main.js
+++ src/main.js
@@ -25,10 +25,12 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 
 library.add(farAngleDown, farAngleUp, farTimes, farPlus, farMinus, faCheck, faCarBus, faFacebook, faTwitter, faInstagram);
 
-// import pinboard
-import pinboard from '@phila/pinboard';
-// import pinboard from '../node_modules/@phila/pinboard/src/main.js';
-import '../node_modules/@phila/pinboard/dist/style.css';
+// use these if running off unlinked package
+// import pinboard from '@phila/pinboard';
+// import '../node_modules/@phila/pinboard/dist/style.css';
+// OR
+// use this if running off linked package
+import pinboard from '../node_modules/@phila/pinboard/src/main.js';
 
 // data-sources
 import ost from './data-sources/ost';
